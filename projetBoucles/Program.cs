7c9fbc8a0bc654a71fad64db4c76f4565fd1069c
Christ ASSANA <christassana@gmail.com>
2024-06-06 15:55:32 +0200

    Update

--- projetBoucles/Program.cs
+++ projetBoucles/Program.cs
@@ -37,7 +37,7 @@ class Program
             {
                 {
                     Console.WriteLine("Pas : " +moves[j]);
-                }
+                                    }
             }
         }
     }
